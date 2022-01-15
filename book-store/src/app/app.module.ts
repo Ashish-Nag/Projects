d28fc0f581e59df03a774a5e7df34264b634a0b0
Ashish Nag <ashish@nag.com>
2022-01-15 21:12:12 +0530

    added routing

--- book-store/src/app/app.module.ts
+++ book-store/src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/components/login/login.component';
+import { RegisterComponent } from './auth/components/register/register.component';
+import { HomeComponent } from './home/components/home/home.component';
 import { HomeModule } from './home/home.module';
 
+// * Define the routes for displaying different components
+const routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signin', component: RegisterComponent}
+]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HomeModule
+    BrowserModule, HomeModule, RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
