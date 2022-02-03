3297fc2674d6cfc8bb8a4dadc4b2e932f86a2aeb
Ashish Nag <ashish@nag.com>
2022-02-03 14:20:19 +0530

    updated components

--- book-store-app/book-store/src/app/app.module.ts
+++ book-store-app/book-store/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { AdminModule } from './admin/admin.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/components/login/login.component';
@@ -8,19 +9,23 @@ import { RegisterComponent } from './auth/components/register/register.component
 import { HomeComponent } from './home/components/home/home.component';
 import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
+import { BookFormComponent } from './admin/components/book-form/book-form.component';
+import { BookListComponent } from './admin/components/book-list/book-list.component';
 
 // * Define the routes for displaying different components
 const routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'signin', component: RegisterComponent}
+  {path: 'signin', component: RegisterComponent},
+  {path: 'admin/bookform', component: BookFormComponent},
+  {path: 'admin/booklist', component: BookListComponent}
 ]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HomeModule, RouterModule.forRoot(routes), SharedModule
+    BrowserModule, HomeModule, AdminModule, RouterModule.forRoot(routes), SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
